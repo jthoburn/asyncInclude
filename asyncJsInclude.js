5f9cfd3d25e9ff84a3667800ec1474966b175aa9
James Thoburn <james@43labs.co>
2013-02-07 13:03:12 -0600

    status=[]v{}, bug fixes, added script dependencies

--- asyncJsInclude.js
+++ asyncJsInclude.js
@@ -3,55 +3,52 @@ window['asyncInclude'] = new function() {
 		var inc = this;
 		
 		//standardized __SCRIPTS__,__FILE__ and __DIR__ references
-		var __STATUS__ = {},
+		var __STATUS__ = [],
 			__SCRIPTS__ = document.getElementsByTagName('script');
+		
+		/*
+			functions/vars for use as an included file or own file when you need location vars
 			
-		var dirname = function(path) {
-   			return path.replace(/\\/g,'/').replace(/\/[^\/]*$/, '');;
-		};
-		
-		var justify = function(path) {
-			var count = (path.match(/\.\.\//g)||[]).length, base = inc['__DIR__'];
-			if( count ) {
-				while( count-- )
-					base = dirname(base);
-				return base + '/' + path.replace(/\.\.\//g,'');
+			var dirname = function(path) {
+   				return path.replace(/\\/g,'/').replace(/\/[^\/]*$/, '');
+			};
+			
+			//make a link absolute v. relative
+			var justify = function(path) {
+				var count = (path.match(/\.\.\//g)||[]).length, base = inc['__DIR__'];
+				if( count ) {
+					while( count-- )
+						base = dirname(base);
+					return base + '/' + path.replace(/\.\.\//g,'');
+				}
+				return path;
 			}
-			return path;
-		}
-		
-		//for use as an included file: inc['__DIR__'] = ( inc['__FILE__'] = __SCRIPTS__[__SCRIPTS__.length-1].src ).split('?')[0].split('/').slice(0, -1).join('/') + '/';
-		inc['__DIR__'] = dirname( inc['__FILE__'] = window.location.href );
-		
-		//add preloaded scripts to status object
-		var index = __SCRIPTS__.length;
-		while(index--) {
-			if( !__STATUS__.hasOwnProperty( justify(__SCRIPTS__[index].src)) )
-				__STATUS__[justify(__SCRIPTS__[index].src)] = { 'src' : __SCRIPTS__[index].src ,'status': 'PREVIOUSLY LOADED' , callback : false};
-		}
+			
+			//included file
+			inc['__DIR__'] = dirname( inc['__FILE__'] = __SCRIPTS__[__SCRIPTS__.length-1].src )) + '/';
+			
+			//embeded script
+			inc['__DIR__'] = dirname( inc['__FILE__'] = window.location.href ) + '/';
+		*/
 		
 		//test dom status, or alternatively set this to true to force scripts to load
 		inc['ready'] = function(){ return !/in/.test(document.readyState);};
 		
 		//allow status checks
-		inc['status'] = function(f) { if(!f) return __STATUS__; return __STATUS__[urlprotocol(f)]['status']; };
+		inc['status'] = function(f) { if(!f) return __STATUS__; var a = __STATUS__.length; while(a--){ if( __STATUS__[a]['src'] == f) return __STATUS__[a]['status'];} };
 		
-		//check if a variable is defined
+		//check if a global is defined
 		function defined(v) {
 			return window.hasOwnProperty(v);
 		};
-	
-		//check is a script's source is present or has previously been requested with .include()
-		function initiated(s) {
-			if(__STATUS__.hasOwnProperty( justify(s) ) )
-				return true;
-			var index = __SCRIPTS__.length;
-			while(index--) {
-				if(__SCRIPTS__[index].src == s) {
-					__STATUS__[justify(s)] = { 'src' : s , 'status' : 'PRE-EXISTING' };
-					return true;
-					}
-				}
+		
+		//check if a script exists in the __STATUS__ array
+		function get(s) {
+			var a = __STATUS__.length;
+			while(a--){
+				if( __STATUS__[a]['src'] == a)
+					return __STATUS__[a];
+			}
 			return false;
 		};
 	
@@ -62,19 +59,57 @@ window['asyncInclude'] = new function() {
 			return f.replace('http:','https:');
 		};
 		
+		//whether we're ready to run a callback
+		var isReady = function(f) {
+			var num = f.dependencies.length, ready = true;	
+			while( num-- ) {
+				if( !window.hasOwnProperty( f.dependencies[num] ) )
+					ready = false;
+				}
+			return ready;
+		};
+		
+		var getByGlobal = function(g) {
+			var a = __STATUS__.length;
+			while(a--){
+				if( __STATUS__[a].global == g)
+					return __STATUS__[a];
+			}
+			return false;
+		};
+		
+		var loadWhenReady = function(f){
+			var num = f.dependencies.length,ref;
+			
+			while( num-- ) {
+				if( !window.hasOwnProperty( f.dependencies[num] ) ) {
+					if(ref = getByGlobal(f.dependencies[num]) ) {
+						if( !!ref.callback )
+							ref.callback = (function(){ ref.callback(); if( isReady(f) ) f.callback(); });
+						else
+							ref.callback = (function(){ if( isReady(f) ) f.callback(); });
+						}
+					else {
+						var TO = function(){isReady(f)?f.callback():setTimeout( (function(){ TO(); }), 15);};
+						TO();
+					}
+				}
+			}
+					
+		};
+		
 		//load script
 		var load = function(f) {
-			if(defined(__STATUS__[f].global))
-				__STATUS__[f]['status'] += "PRE-EXISTING";
+			if( defined(f.global) )
+				f['status'] += "PRE-EXISTING";
 			else {
 				var s = document.createElement('script');
 				s.type = 'text/javascript';
 				s.async = true;
-				s.src = urlprotocol(f);
+				s.src = urlprotocol(f['src']);
 				__SCRIPTS__[0].parentNode.insertBefore(s,__SCRIPTS__[0]);
-				__SCRIPTS__.push(s);
-				__STATUS__[f]['status'] += ' LOADING';
-				s.onload= function(){ __STATUS__[f]['status'] += ' OK'; if( !! __STATUS__[f].callback ) __STATUS__[f].callback(); };
+				f['status'] += ' LOADING';
+				s.onload= function(){ f['status'] += ' OK'; if( !!f.callback ) !f.dependencies?f.callback():loadWhenReady(f); };
 			}
 		};
 		
@@ -82,49 +117,62 @@ window['asyncInclude'] = new function() {
 			var s = document.createElement('link');
 			s.type='text/css';
 			s.rel='stylesheet';
-			s.href=urlprotocol(f);
+			s.href=urlprotocol(f['src']);
 			__SCRIPTS__[0].parentNode.insertBefore(s,__SCRIPTS__[0]);
-			__STATUS__[f]['status'] += ' LOADING';
-			s.onload= function(){ __STATUS__[f]['status'] += ' OK'; if( !! __STATUS__[f].callback ) __STATUS__[f].callback(); };
+			f['status'] += ' LOADING';
+			s.onload= function(){ f['status'] += ' OK'; if( !! f.callback ) f.callback(); };
 		};
 		
 		//push unique script's to __status__
-		var push = function(f,c,b,d) { 
-			if( (d == 'css' || !defined(b)) && !initiated(f) ) {
-				__STATUS__[justify(f)] = { 'src' : f, 'status' : 'WAITING', callback : c , global : b , type : d };
+		var push = function(f,c,b,d,e) {
+			var s = get(f);
+			if( !s && (d == 'css' || !defined(b)) ) {
+				__STATUS__.push({ 'src' : f, 'status' : 'WAITING', callback : c , global : b , type : d , dependencies : e });
 				if( inc['ready']() ) { 
-					if( d == 'js' ) load(f);
-					else loadCSS(f);
+					if( d == 'js' ) load(get(f));
+					else loadCSS(get(f));
 				}
 			}
-			else {
-				__STATUS__[justify(f)]['status'] += " REJECTED"; };
+			else
+				if(s) s['status'] += " REJECTED";
+		};
+		
+		
+		//add preloaded scripts to status object
+		var index = __SCRIPTS__.length;
+		while(index--) {
+			if( __SCRIPTS__[index]['src'] != '' && !get(__SCRIPTS__[index]['src']) ) {
+				push( __SCRIPTS__[index]['src'], false, '', 'js' );
+				(get(__SCRIPTS__[index]['src']))['status'] = 'PRE-EXISTING';
 				}
+		}
+		
+		
 		
 		//our collection function for adding scripts to the loader
-		inc['include'] = function(f,c,b,d) {
+		inc['include'] = function(f,c,b,d,e) {
 			if(Object.prototype.toString.call(f) == '[object Array]') {
 				var a = f.length;
 				for(var o=0;o<a;o++)
-					push(f[o][0],f[o][1],f[o][2],f[o][3]);
+					push(f[o][0],f[o][1],f[o][2],f[o][3],f[o][4]);
 					
 			}
 			else
-				push(f,c,b,d);
+				push(f,c,b,d,e);
 		};
 	
 		//document ready callback: see http://dustindiaz.com/smallest-domready-ever , alternative is comment #217 here http://dean.edwards.name/weblog/2006/06/again/#comment367184 
-		function r(f){!inc['ready']()?setTimeout((function(){ r(f); }),9):f()};
+		function r(f){ !inc['ready']()?setTimeout((function(){ r(f); }),15):f()};
 	
 		//load everything once the page is ready	
-		r(function(){
+		r( (function(){
 			var length = __STATUS__.length;
-			for(i = 0, j=__STATUS__[i]; i < length; i++) {
-				if( j['status'].indexOf('LOADING') === -1 ) {
-					if(j.type == 'js')	load(j.src);
-					else	loadCSS(j.src);
+			for(i = 0; i < length; i++) {
+				if( __STATUS__[i]['status'].indexOf('LOADING') === -1 ) {
+					if(__STATUS__[i].type == 'js')	load(__STATUS__[i]);
+					else	loadCSS(__STATUS__[i]);
 					}
 				}
-			});
+			}) );
 	
 	};
